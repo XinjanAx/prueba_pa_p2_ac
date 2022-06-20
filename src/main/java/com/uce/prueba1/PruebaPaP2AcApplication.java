0d75cc37847de9e4bf94e2c61cfe5a8c386bdf8c
AlexCondor <alexguerron@gmail.com>
2022-06-20 18:22:00 -0500

    commit2

---
+++ src/main/java/com/uce/prueba1/PruebaPaP2AcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.prueba1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPaP2AcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPaP2AcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/prueba1/PruebaPaP2AcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.prueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPaP2AcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
